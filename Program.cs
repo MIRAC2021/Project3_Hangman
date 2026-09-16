33d886f7bbe0506258329e94610615e440bc8b02
Mehak Chopra <Mehak.Chopra@ccam-va.com>
2026-09-16 00:20:50 -0400

    initialized code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hangman!");
+
+            List <string> Words = new List<string> { "apple", "banana", "cherry", "date", "elderberry" };
+
+            Console.WriteLine("Please enter a word for the player to guess:");
+
+            // to be continued;
+
+        }
+    }
+}
